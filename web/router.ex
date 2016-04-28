2a24c0363aecaf6abb2e59aae2501286423e7dc6
sashman <sashman90@gmail.com>
2016-04-28 17:31:28 +0100

    Changing to post and removing CSRF

--- web/router.ex
+++ web/router.ex
@@ -5,7 +5,7 @@ defmodule SlackingPhoenix.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
+    # plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
@@ -18,7 +18,7 @@ defmodule SlackingPhoenix.Router do
 
     get "/", PageController, :index
 
-    get "/anonymize", PageController, :anonymize
+    post "/anonymize", PageController, :anonymize
   end
 
   # Other scopes may use custom stacks.
