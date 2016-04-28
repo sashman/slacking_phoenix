437fbb554aa12c42f6cdf55d818251fd80a4d470
sashman <sashman90@gmail.com>
2016-04-28 16:26:43 +0100

    Adding new anonymize action

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule SlackingPhoenix.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def anonymize(conn, _params) do
+    json conn, %{ text: _params["text"], response_type: "in_channel" }
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,8 @@ defmodule SlackingPhoenix.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
+    get "/anonymize", PageController, :anonymize
   end
 
   # Other scopes may use custom stacks.
